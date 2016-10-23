0fb3b982be83ea7d56ea2171c103095e98a165b9
Edele Gizatullin <me@edele.ru>
2016-10-24 00:26:09 +0500

    Sorted houses by wins

--- scripts/HousesStatsVisualizer.js
+++ scripts/HousesStatsVisualizer.js
@@ -9,14 +9,14 @@ class HousesStatsVisualizer {
     showTo(el) {
         let html = '<div class=clearfix>';
 
-        objectEach(this.data, (stats, name) => {
+        this.data.forEach(house => {
             html += `
                 <div class='house-stat clearfix'>
-                    <img class=house-stat__gerb src="gerbs/${name}.png"/>
+                    <img class=house-stat__gerb src="gerbs/${house.name}.png"/>
                     <div class="house-stat__text">
-                        <div class=house-stat__name>${name}</div>
-                        <span class=house-stat__wins>${stats.winsCount}</span>
-                        <span class=house-stat__total>/${stats.gamesCount}</span>
+                        <div class=house-stat__name>${house.name}</div>
+                        <span class=house-stat__wins>${house.winsCount}</span>
+                        <span class=house-stat__total>/${house.gamesCount}</span>
                     </div>
                 </div>
             `
